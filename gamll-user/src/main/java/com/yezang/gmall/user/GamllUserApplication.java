fad9ed24ec81e9c0f0389356a2983303a6f5218b
49938 <4993820@qq.com>
2020-06-15 14:12:46 +0800

    提交测试

---
+++ gamll-user/src/main/java/com/yezang/gmall/user/GamllUserApplication.java
@@ -0,0 +1,13 @@
+package com.yezang.gmall.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamllUserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GamllUserApplication.class, args);
+	}
+
+}
